d17c1e872c9afa2801a45cff2f6e759cf0eb3c17
nnkgw <renatus_novus_x@outlook.jp>
2025-08-16 11:29:19 +0900

    add PORT_STRING

--- src/sample.c
+++ src/sample.c
@@ -3,15 +3,16 @@
 
 #include "mdpad.h"
 
-const char* PAD_TYPE[] = { "ATARI", "MD3B", "MD6B" };
+const char* PORT_STRING[] = { "JOY STICK 1", "JOY STICK 2"};
+const char* PAD_STRING[] = { "ATARI", "MD3B", "MD6B" };
 
-int main(void) {
+int main(int argc, char* argv[]) {
   int usp = _iocs_b_super(0);
   while(1){
     mdpad_type_t t = mdpad_detect(MDPAD_PORT_A);
     mdpad_state_t st = (t == MDPAD_MD6) ? mdpad_read6(MDPAD_PORT_A)
                                         : mdpad_read3(MDPAD_PORT_A);
-    printf("\r[%s] bits=0x%04X        ", PAD_TYPE[(int)t], st.bits);
+    printf("\r[%s][%s] bits=0x%04X        ", PORT_STRING[MDPAD_PORT_A], PAD_STRING[(int)t], st.bits);
   }
   _iocs_b_super(usp);
   return 0;
